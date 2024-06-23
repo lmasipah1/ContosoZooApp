c6bdd2bf68f2030b6854ac22727795818c923eae
lmasipah1 <lmasipah1@gmail.com>
2024-06-23 16:58:51 +0200

    final console app

--- Program.cs
+++ Program.cs
@@ -9,11 +9,23 @@ string[] pettingZoo =
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
 
+PlanSchoolVisit("School A");
+PlanSchoolVisit("School B", 3);
+PlanSchoolVisit("School C", 2);
+
+void PlanSchoolVisit(string schoolName, int groups = 6)
+{
+    RandomizeAnimals();
+    string[,] group = AssignGroup(groups);
+    Console.WriteLine(schoolName);
+    PrintGroup(group);
+}
+
 RandomizeAnimals();
-// string[,] group = AssignGroup();
+string[,] group = AssignGroup();
 Console.WriteLine("School A");
 Console.WriteLine();
-// PrintGroup(group);
+PrintGroup(group);
 
 void RandomizeAnimals()
 {
@@ -26,5 +38,34 @@ void RandomizeAnimals()
         string temp = pettingZoo[i];
         pettingZoo[i] = pettingZoo[r];
         pettingZoo[r] = temp;
+    }
+}
+
+string[,] AssignGroup(int groups = 6) 
+{
+    string[,] result = new string[groups, pettingZoo.Length/groups];
+
+    for (int i = 0; i < groups; i++)
+    {
+        int start = 0;
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i,j] = pettingZoo[start++];
+        }
+    }
+
+    return result;
+}
+
+void PrintGroup(string[,] group) 
+{
+    for (int i = 0; i < group.GetLength(0); i++) 
+    {
+        Console.Write($"Group {i + 1}: ");
+        for (int j = 0; j < group.GetLength(1); j++) 
+        {
+            Console.Write($"{group[i,j]}  ");
+        }
+        Console.WriteLine();
     }
 }
--- obj/Debug/net8.0/Methods.AssemblyInfo.cs
+++ obj/Debug/net8.0/Methods.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Methods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3535354c65deebb8099bfec3673810b672287618")]
 [assembly: System.Reflection.AssemblyProductAttribute("Methods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Methods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
